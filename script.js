4a490b3a02e0a6b96eafd18e46e2190ba988d489
ferrarinha <126687261+ferrarinha@users.noreply.github.com>
2024-06-10 11:41:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "O que é bullying?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "A averssão ao comportamento violento.",
+            "É um comportamento agresssivo repetitivo que causa grandes traumas."
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Quais são os tipos comuns de bullying nas escolas?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Físico, verbal, social e cyberbullying.",
+            "Abraçar, agradecer o colega."
         ]
     },
     {
-        enunciado: "Pergunta 3",
+        enunciado: "Como o bullying afeta as pessoas?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Pode causar danos emocionais, psicológicos e físicos.",
+            "O bullying não afeta após parar de ocorrer."
         ]
     },
     {
-        enunciado: "Pergunta 4",
+        enunciado: "Como as escolas lidam com o bullying?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "As escolas não possuem alternativas para reverter os casos de bullying.",
+            "As escolas tem políticas anti-bullying e programas de prevenção para o acaso."
         ]
     },
     {
-        enunciado: "Pergunta 5",
+        enunciado: "Quais são os desafios enfrentados na prevenção do bullying nas escolas?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Falta de conscietização sobre o bullying, relutância dos alunos em relatarem os incidentes e a falta de recursos.",
+            "Não possuem desafios, pois todos compreendem a gravidade do acaso."
         ]
     }
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
